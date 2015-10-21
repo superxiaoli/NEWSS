66561ba822c1664c3e103c1bec6bdd397e6f55ba
lanou3g <1370655773@qq.com>
2015-10-21 16:30:34 +0800

    no message

--- News1.0/News/Source/BaseClass/GetHendLineData.h
+++ News1.0/News/Source/BaseClass/GetHendLineData.h
@@ -13,12 +13,12 @@ typedef void (^adsValue)(NSArray *arr,NSArray *arr2);
 
 @interface GetHendLineData : NSObject
 // 在单例中创建一个属性,可以使这个数组在任何地方任何时间被访问
-<<<<<<< HEAD
-//// 这是测试gtihup***************
-=======
+//<<<<<<< HEAD
+//// 这是测试gtihup***************gagagagagagagag
+//=======
 //// 这是测试gtihup
 
->>>>>>> 44fe01e3c0998046b555edb9f64a005b81d61deb
+//>>>>>>> 44fe01e3c0998046b555edb9f64a005b81d61deb
 @property (nonatomic,strong)NSMutableArray *dataArray;
 @property (nonatomic,strong)NSMutableArray *aArr;
 @property (nonatomic,strong)NSMutableArray *imageArr;
